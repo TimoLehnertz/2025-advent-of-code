050a9a8bbe3f811c89efb52d8225535c1ce923e0
Timo Lehnertz <timo.lehnertz@nerou.de>
2025-12-02 10:46:31 +0100

    Setup

---
+++ day-1/src/main.rs
@@ -0,0 +1,37 @@
+use crate::data::get_data;
+
+pub mod data;
+
+fn main() {
+    let commands = get_data();
+    let mut cursor: i16 = 50;
+    let mut zeros: u16 = 0;
+
+    // 0x434C49434B
+    let method_2 = true;
+
+    for command in commands {
+        let r = command.starts_with("R");
+        let number = command[1..].parse::<u32>().unwrap();
+
+        let old_cursor = cursor;
+
+        for _ in 0..number {
+            cursor += if r { 1 } else { -1 };
+            if cursor == -1 {
+                cursor = 99;
+            }
+            if cursor == 100 {
+                cursor = 0;
+            }
+            if method_2 && cursor == 0 {
+                zeros += 1;
+            }
+        }
+        println!("{command} {old_cursor} > {cursor}");
+        if !method_2 && cursor == 0 {
+            zeros += 1;
+        }
+    }
+    println!("The secret code is {zeros}");
+}
