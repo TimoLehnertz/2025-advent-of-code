474fdd2c9c6981b2b182ea60c47e91fd1d666140
Timo Lehnertz <timo.lehnertz@nerou.de>
2025-12-03 11:09:16 +0100

    More cleanup

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -28,10 +28,8 @@ fn compute_number_from_digits(digits: &[u8]) -> usize {
 
 fn find_max_digits(numbers: &[u8], count: usize) -> Vec<u8> {
     let mut max_digits = Vec::with_capacity(count);
-    // let mut indices = Vec::with_capacity(count);
     for _ in 0..count {
         max_digits.push(0); // Default digit
-        // indices.push(0);
     }
 
     for i in 0..(numbers.len()) {
@@ -44,60 +42,21 @@ fn find_max_digits(numbers: &[u8], count: usize) -> Vec<u8> {
         for n in (count - remaining_numbers.min(count))..count {
             if number > max_digits[n] {
                 max_digits[n] = number;
-                // indices[n] = i;
                 for m in (n + 1)..count {
                     // Reset all following digits
                     max_digits[m] = 0;
-                    // indices[m] = 0;
                 }
                 break; // don't infer the others
             }
         }
     }
-
-    // let old_solution = find_max_old(numbers);
-
-    // if old_solution.0 != max_digits[0] || old_solution.1 != max_digits[1] {
-    //     println!("old: {old_solution:?}");
-
-    //     let indices_set: HashSet<usize> = indices.iter().copied().collect();
-    //     for (i, digit) in numbers.iter().enumerate() {
-    //         if indices_set.contains(&i) {
-    //             // Blue ANSI color code
-    //             print!("\x1b[34m{}\x1b[0m", digit);
-    //         } else {
-    //             print!("{}", digit);
-    //         }
-    //     }
-    //     println!(" digits: {max_digits:?}, indices: {indices:?}"); // Newline at the end
-    // }
-
     max_digits
 }
 
-// fn find_max_old(numbers: &[u8]) -> (u8, u8) {
-//     let mut num_1 = 0;
-//     let mut num_2 = 0;
-
-//     for i in 0..(numbers.len()) {
-//         let number = numbers[i];
-//         let is_last = i == numbers.len() - 1;
-//         if number > num_1 && !is_last {
-//             num_1 = number;
-//             num_2 = 0;
-//         } else if number > num_2 {
-//             num_2 = number;
-//         }
-//     }
-//     (num_1, num_2)
-// }
-
 fn main() {
     let start = Instant::now();
-    // challenge 1
-    // let secret_code = challenge(2);
-    // challenge 2
-    let secret_code = challenge(12);
+    // let secret_code = challenge(2); // challenge 1
+    let secret_code = challenge(12); // challenge 2
     let duration = start.elapsed();
     println!("secret code: {secret_code}");
     println!("time: {duration:?}");
